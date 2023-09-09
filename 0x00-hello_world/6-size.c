d785f5aa0a4c769358ab94aeb6186bdc3c0bfa1d
Orevaoghene <oreva.raphael@gmail.com>
2023-09-09 15:23:53 -0700

    working hard

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,6 @@
 /**
  * main - The entry point that the compiler needs to run the code
  *
- * Description - A program that finds the size in bytes of a data type or expression
- *
  * Retrun: 0 (success)
  */
 int main(void)
@@ -11,6 +9,7 @@ int main(void)
 	printf("Size of a char: = %d byte(s)\n", sizeof(char));
 	printf("Size of an int: = %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: = %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: =%lld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: = %d byte(s)\n", sizeof(float));
 	return (0);
 }

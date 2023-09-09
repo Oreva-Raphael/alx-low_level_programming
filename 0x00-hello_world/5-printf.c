329289d0a48018bda335f39501aa3a2a7f957eba
Orevaoghene <oreva.raphael@gmail.com>
2023-09-09 11:36:40 -0700

    getting this one right

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - All other functions are called from here 
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

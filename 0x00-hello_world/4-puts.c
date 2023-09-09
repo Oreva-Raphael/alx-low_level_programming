71b58e19433e99fef27a55bf5b7e51f68dc7c10e
Orevaoghene <oreva.raphael@gmail.com>
2023-09-09 10:24:45 -0700

    Fixing problems

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 /**
  * main - The start of the code 
  *
- * Description - Using the puts function to print out a text
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

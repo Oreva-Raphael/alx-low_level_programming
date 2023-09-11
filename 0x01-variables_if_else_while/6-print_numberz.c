8cf8e1131eac44837bdbcf9bfbf559a322d19b5f
Orevaoghene <oreva.raphael@gmail.com>
2023-09-11 09:21:01 -0700

    It should work now

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int num = 10;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+	}
+	putchar('\n');
+	return(0);
+}

8a8bb37302e430edc65b3788e963331414f4f9d8
Orevaoghene <oreva.raphael@gmail.com>
2023-09-11 08:31:48 -0700

    I have fixed it

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int num = 10;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}

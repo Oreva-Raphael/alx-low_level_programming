d0c40cc0fd08c0262064c3f1f3748e932fe679c3
Orevaoghene <oreva.raphael@gmail.com>
2023-09-11 08:22:55 -0700

    I did this one on my own

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - All other functions are called here
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	int num = 10;
+	
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d",num);
+	}
+	printf("\n");
+	return (0);
+}	

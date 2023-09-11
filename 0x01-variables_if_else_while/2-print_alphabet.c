6d5d5a561f705c860f71cb75afbb2ca3dc704522
Orevaoghene <oreva.raphael@gmail.com>
2023-09-11 06:29:27 -0700

    Printing alphabets using char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char letters = 'a';
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		putchar(letters);
+	}
+	putchar('\n');
+	return (0);
+}

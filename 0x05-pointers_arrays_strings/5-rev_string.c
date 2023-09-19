dd78c232d4ffa0286a520b350da1b936381c1b5f
Orevaoghene <oreva.raphael@gmail.com>
2023-09-18 19:48:51 -0700

    Annoying

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: catatonic
+ */
+void rev_string(char* s)
+{
+
+	int len, left, right;
+	char temp;
+
+	len = 0;
+/* checking length of string */
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+/* index of the beginning of string from right */
+	right = len - 1;
+
+/* swapping indexes from left to right of the middle */
+	for (left = 0; right >= 0 && left < right; left++, right--)
+	{
+		temp = s[left];
+		s[left] = s[right];
+		s[right] = temp;
+	}
+
+}

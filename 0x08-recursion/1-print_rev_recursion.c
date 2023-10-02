feb2f168b5358344db042ded93d224f2f23abdfb
Orevaoghene <oreva.raphael@gmail.com>
2023-10-01 18:20:40 -0700

    Getting to know it a little

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - printing the reverse recursion
+ * @s: the character of each string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	 /* this recursively goes to the end of the string */
+	_print_rev_recursion(s + 1);
+	/* this prints from the end to the beginning */
+	_putchar(*s);
+}

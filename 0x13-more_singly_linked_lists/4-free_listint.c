372870f804791d0922979388071f471491c66a14
Orevaoghene <orevaoghene.raphael@gmail.com>
2023-12-12 19:32:32 -0800

    Commitig is getting tiring

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - frees a listint_t list
+ * @head: the start of the list
+ * Retur: nothing, void.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
